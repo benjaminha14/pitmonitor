d2bcdec228513b1e0bf3f026a8b82abd9d33df51
benjaminha14 <benjaminha14@gmail.com>
2016-01-25 19:22:51 -0800

    Init file

---
+++ workspace/js/app.js
@@ -0,0 +1,13 @@
+function select() {
+    
+    $('#pages').
+    
+    // var linkedDoc = info.document;
+    // var cssLink = linkedDoc.head.createElement("link");
+    // cssLink.href = "../css/linkedDynamCSS.css"; 
+    // cssLink.rel = "stylesheet";
+    // cssLink.type = "text/css";
+    // linkedDoc.head.appendChild(cssLink);
+}
+
+
